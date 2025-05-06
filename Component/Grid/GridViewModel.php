65506468fd9443ffac3362a14a22d276bf983f79
Jisse Reitsma <jisse@yireo.com>
2025-05-06 09:37:13 +0200

    Refactoring

--- Component/Grid/GridViewModel.php
+++ Component/Grid/GridViewModel.php
@@ -3,26 +3,11 @@
 namespace YireoTraining\ExampleLokiAdminProducts\Component\Grid;
 
 use Magento\Framework\DataObject;
-use Magento\Framework\UrlFactory;
 use Yireo\LokiAdminComponents\Component\Grid\GridViewModel as OriginalGridViewModel;
 use Yireo\LokiAdminComponents\Grid\Cell\CellAction;
-use Yireo\LokiAdminComponents\Grid\Cell\CellActionFactory;
-use Yireo\LokiAdminComponents\Grid\State;
-use Yireo\LokiComponents\Util\CamelCaseConvertor;
-use YireoTraining\ExampleLokiAdminProducts\Util\ColumnLoader;
 
 class GridViewModel extends OriginalGridViewModel
 {
-    public function __construct(
-        State $state,
-        UrlFactory $urlFactory,
-        CellActionFactory $cellActionFactory,
-        CamelCaseConvertor $camelCaseConvertor,
-        private ColumnLoader $columnLoader,
-    ) {
-        parent::__construct($state, $urlFactory, $cellActionFactory, $camelCaseConvertor);
-    }
-
     public function getIndexUrl(): string
     {
         return $this->urlFactory->create()->getUrl('loki_admin_products/index/grid');
@@ -31,11 +16,6 @@ class GridViewModel extends OriginalGridViewModel
     public function getNewUrl(): string
     {
         return $this->urlFactory->create()->getUrl('loki_admin_products/index/form');
-    }
-
-    public function getColumns(): array
-    {
-        return $this->columnLoader->getColumns();
     }
 
     public function getRowAction(DataObject $item): CellAction
