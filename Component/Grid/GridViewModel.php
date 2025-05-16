6886ce25944f58f3763fb04037ed9549c995f12c
Jisse Reitsma <jisse@yireo.com>
2025-05-16 14:05:30 +0200

    Allow for cell templates to be defined via XML layout

--- Component/Grid/GridViewModel.php
+++ Component/Grid/GridViewModel.php
@@ -3,21 +3,25 @@
 namespace YireoTraining\ExampleLokiAdminProducts\Component\Grid;
 
 use Magento\Framework\DataObject;
+use Override;
 use Yireo\LokiAdminComponents\Component\Grid\GridViewModel as OriginalGridViewModel;
 use Yireo\LokiAdminComponents\Grid\Cell\CellAction;
 
 class GridViewModel extends OriginalGridViewModel
 {
+    #[Override]
     public function getIndexUrl(): string
     {
         return $this->urlFactory->create()->getUrl('loki_admin_products/index/grid');
     }
 
+    #[Override]
     public function getNewUrl(): string
     {
         return $this->urlFactory->create()->getUrl('loki_admin_products/index/form');
     }
 
+    #[Override]
     public function getRowAction(DataObject $item): CellAction
     {
         $editUrl = $this->urlFactory->create()->getUrl('catalog/product/edit', [
@@ -27,9 +31,9 @@ class GridViewModel extends OriginalGridViewModel
         return $this->cellActionFactory->create($editUrl, 'Edit');
     }
 
+    #[Override]
     public function getSearchableFields(): array
     {
-        // @todo: Replace this with automatic logic of product entity
         return [
             'name',
             'description',
@@ -37,6 +41,7 @@ class GridViewModel extends OriginalGridViewModel
         ];
     }
 
+    #[Override]
     protected function getAdditionalActions(DataObject $item): array
     {
         $cellActions = [];
